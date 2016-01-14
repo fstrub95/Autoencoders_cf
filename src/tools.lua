0d82847fb1cce18e5806ffa2febe0d9f9ad15974
fstrub <florian.strub@gmail.com>
2016-01-14 15:13:07 +0100

    use better signature for tools

--- src/tools.lua
+++ src/tools.lua
@@ -3,15 +3,8 @@ require("torch")
 ---------------------------------------
 -- Basic Constant 
 
-NaN =  1/0 -- -999
 
-
-function uid()
-   return (os.time() .. math.random()):gsub('%.','')
-end
-
-
-function round(num, idp)
+math.round = math.round or function(num, idp)
    
   if idp == nil then idp = 4 end
 
@@ -20,7 +13,7 @@ function round(num, idp)
 end
 
 
-function merge(t1,  t2)
+table.merge = table.merge or function(t1,  t2)
     for k, v in pairs(t2) do
         if (type(v) == "table") and (type(t1[k] or false) == "table") then
             merge(t1[k], t2[k])
@@ -33,12 +26,31 @@ function merge(t1,  t2)
     return t1
 end
 
-function string.starts(String,Start)
+string.starts = string.starts or function (String,Start)
    return string.sub(String,1,string.len(Start))==Start
 end
 
-function string.ends(String,End)
+string.ends = string.ends or function(String,End)
    return End=='' or string.sub(String,-string.len(End))==End
+end
+
+string.split = string.split or function(str, pat) 
+   local t = {}  -- NOTE: use {n = 0} in Lua-5.0
+   local fpat = "(.-)" .. pat
+   local last_end = 1
+   local s, e, cap = str:find(fpat, 1)
+   while s do
+      if s ~= 1 or cap ~= "" then
+    table.insert(t,cap)
+      end
+      last_end = e+1
+      s, e, cap = str:find(fpat, last_end)
+   end
+   if last_end <= #str then
+      cap = str:sub(last_end)
+      table.insert(t, cap)
+   end
+   return t
 end
 
 function GetSize(X,dim)
@@ -49,6 +61,8 @@ function GetSize(X,dim)
       return X:size(dim)
    end
 end
+
+
 
 function tensorToCsv(M, outFile)
 
