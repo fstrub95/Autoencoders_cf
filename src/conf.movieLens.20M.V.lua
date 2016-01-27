82a7851acb4c144da13775de82f80a63126c333e
fstrub95 <florian.strub@gmail.com>
2016-01-28 00:04:55 +0100

    add script to launch numerous xp

---
+++ src/conf.movieLens.20M.V.lua
@@ -0,0 +1,28 @@
+dofile("SDAECriterionGPU.lua")
+
+configV = 
+{
+   useMetadata = false,
+   layer1 = 
+   {      
+      layerSize = 584,
+      { 
+         criterion = nnsparse.SDAECriterionGPU(nn.MSECriterion(),
+         {
+            alpha = 0.98905390398577,
+            beta  = 0.6047364819312,
+            hideRatio = 0.24630973351843,
+         }), 
+         noEpoch = 15, 
+         miniBatchSize = 35,
+         learningRate = 0.050508657319437 ,  
+         learningRateDecay = 0.35850763596698,
+         weightDecay = 0.026518716275071,
+      },
+      
+   },
+ 
+}
+
+
+
---
+++ src/createData.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+noDataset=5
+ratio=0.8
+folder=data-${ratio}
+
+
+mkdir ${folder}
+mkdir ${folder}/1M
+mkdir ${folder}/10M
+mkdir ${folder}/20M
+
+
+
+#movieLens-1M
+for i in `seq 1 $noDataset`
+do
+  th data.lua -ratio ${ratio} -fileType movieLens -seed ${i} -ratings ../data/movieLens/ratings-1M.dat -metaUser ../data/movieLens/users-1M.dat -metaItem ../data/movieLens/movies-1M.dat -out ${folder}/1M/${i}.t7
+done
+
+#movieLens-10M
+for i in `seq 1 $noDataset`
+do
+  th data.lua -ratio ${ratio} -fileType movieLens -seed ${i} -ratings ../data/movieLens-10M/ratings.dat -metaUser "" -metaItem "" -out ${folder}/10M/${i}.t7
+done
+
+#movieLens-20M
+for i in `seq 1 $noDataset`
+do
+  th data.lua -ratio ${ratio} -fileType movieLens -seed ${i} -ratings ../data/movieLens-20M/ratings.csv -metaUser "" -metaItem "" -out ${folder}/20M/${i}.t7
+done
