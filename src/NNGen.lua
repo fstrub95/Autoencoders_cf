dd7d174f25ea6423c3a655a3510c27cde3b2f7d1
root <root@burns.cluster.lille.inria.fr>
2016-01-28 00:07:19 +0100

    enable single thread with NNGen

--- src/NNGen.lua
+++ src/NNGen.lua
@@ -1,32 +1,40 @@
 local NNGen, parent = torch.class("NNGen", "AlgoGen")
-
 local threads = require 'threads'
 
 function NNGen:GenerateOne()           
 
-   self.nnType = "V"
+   self.nnType = NN_TYPE
 
    local gene = {}
-   gene.layer1       = 700--torch.random (400 , 600)
+   gene.layer1       = torch.random (500 , 700)
    gene.alpha1       = torch.uniform(0.8 , 1.2)
-   gene.beta1        = torch.uniform(0.5 , 1)
+   gene.beta1        = torch.uniform(0   , 1)
    gene.hide1        = torch.uniform(0   , 0.5)
-   gene.batch1       = torch.random(10, 50)
-   gene.lrt1         = torch.uniform(0.1, 0.0001)
-   gene.lrtDecay1    = torch.uniform(-0.2, 0.4)
-   gene.weigthDecay1 = torch.uniform(0.1, 0.0001)
-   --gene.grad1        = "sgd"
+--   gene.batch1       = torch.random(10, 50)
+   gene.lrt1         = torch.uniform(0, 0.1)
+   gene.lrtDecay1    = torch.uniform(0, 0.6)
+   gene.weigthDecay1 = torch.uniform(0, 0.1)
+   --gene.grad1      = "sgd"
 
-   gene.layer2       = 500--torch.random (500 , 800)
-   gene.alpha2       = torch.uniform(0.8 , 1.2)
-   gene.beta2        = torch.uniform(0.5 , 1)
-   gene.hide2        = torch.uniform(0   , 0.5)
-   gene.batch2       = torch.random(10, 50)
-   gene.lrt2         = torch.uniform(0.1, 0.0001)
-   gene.lrtDecay2    = torch.uniform(-0.2, 0.4)
-   gene.weigthDecay2 = torch.uniform(0.1, 0.0001)
-   --gene.grad2        = "sgd"
+--   gene.layer2       = torch.random (400 , 600)
+--   gene.alpha2       = torch.uniform(0.8 , 1.2)
+--   gene.beta2        = torch.uniform(0.  , 1)
+--   gene.hide2        = torch.uniform(0   , 0.5)
+--   gene.batch2       = 25--torch.random(10, 50)
+--   gene.lrt2         = torch.uniform(0, 0.1)
+--   gene.lrtDecay2    = torch.uniform(-0.2, 0.4)
+--   gene.weigthDecay2 = torch.uniform(0, 0.1)
+--   gene.grad2        = "sgd"
 
+
+--   gene.alpha3       = torch.uniform(0.8 , 1.2)
+--   gene.beta3        = torch.uniform(0   , 1)
+--   gene.noiseRatio3  = torch.uniform(0   , 1)
+--   gene.noiseStd3    = torch.uniform(0   , 0.5)
+--   gene.weigthDecay3 = torch.uniform(0, 0.5)
+--   gene.lrt3         = torch.uniform(0, 0.5)
+
+--   self:PrintOne(gene)
 
    return gene
 end
@@ -46,8 +54,8 @@ function NNGen:LoadGene(gene)
                      beta      = gene.beta1,
                      hideRatio = gene.hide1,
                   }), 
-               noEpoch           = 10, 
-               miniBatchSize     = gene.batch1,
+               noEpoch           = 8, 
+               miniBatchSize     = 35, -- gene.batch1,
                learningRate      = gene.lrt1,  
                learningRateDecay = gene.lrtDecay1,
                weightDecay       = gene.weigthDecay1,
@@ -57,35 +65,34 @@ function NNGen:LoadGene(gene)
 
          layer2 = 
          {
-            layerSize = gene.layer2,
+            layerSize = 1, --gene.layer2,
             { 
                criterion = nnsparse.SDAECriterionGPU(nn.MSECriterion(),
                   {
-                     alpha = 1,
-                     beta  = 0.8,
-                     noiseRatio = 0.2,
-                     noiseStd  = 0.02, 
+                     alpha = gene.alpha3 or 0,
+                     beta  = gene.beta3 or 0,
+                     noiseRatio = gene.noiseRatio3 or 0,
+                     noiseStd  = gene.noiseStd3 or 0, 
                   }),
-               noEpoch = 4, 
+               noEpoch = 0, 
                miniBatchSize = 20,
-               learningRate  = 5e-5,  
-               learningRateDecay = 0.1,
-               weightDecay = 0.2,
+               learningRate  = gene.lrt3 or 0,  
+               weightDecay   = gene.weigthDecay3 or 0,
                momentum = 0.8
             },
 
             {
                criterion = nnsparse.SDAECriterionGPU(nn.MSECriterion(),
                   {
-                     alpha     = gene.alpha2,
-                     beta      = gene.beta2,
-                     hideRatio = gene.hide2,
+                     alpha     = gene.alpha2 or 0,
+                     beta      = gene.beta2 or 0,
+                     hideRatio = gene.hide2 or 0,
                   }), 
-               noEpoch           = 8, 
-               miniBatchSize     = gene.batch2,
-               learningRate      = gene.lrt2,  
-               learningRateDecay = gene.lrtDecay2,
-               weightDecay       = gene.weigthDecay2,
+               noEpoch           = 0, 
+               miniBatchSize     = 25, -- gene.batch2,
+               learningRate      = gene.lrt2 or 0,  
+               learningRateDecay = gene.lrtDecay2 or 0,
+               weightDecay       = gene.weigthDecay2 or 0,
             },
          },
 
@@ -99,7 +106,69 @@ end
 
 function NNGen:EvaluateAll(genes)      
 
-   local nthread = 2 
+   local nthread = 0 
+
+   if nthread == 0 then
+
+      if torch.type(self.train.U.data[1]) ~= "torch.CudaTensor" then
+         require("cunn")
+   
+         cutorch.setDevice(GPU_DEVICE)
+ 
+         local function toGPU(type)
+            local _train = self.train[type]
+            local _test  = self.test [type]
+
+            for k, _ in pairs(_train.data) do
+
+               self.train[type].data[k] = _train.data[k]:cuda()
+
+               if self.train[type].info.metaDim then
+                  self.train[type].full               = _train.info[k].full:cuda()
+                  self.train[type].info[k].fullSparse = _train.info[k].fullSparse:cuda()
+               end
+            end
+
+            for k, _ in pairs(_test.data) do
+
+               self.test[type].data[k] = _test.data[k]:cuda()
+
+               if self.test[type].info.metaDim then
+                  self.test[type].full               = _test.info[k].full:cuda()
+                  self.test[type].info[k].fullSparse = _test.info[k].fullSparse:cuda()
+               end
+            end
+
+         end
+
+         print("Load representation : " .. NN_TYPE)
+         toGPU(NN_TYPE)
+
+      end
+
+      local noGenes = table.Count(genes)
+      for i = 1,noGenes do
+         print()
+         print("New Gene : " .. i)
+         local gene = genes[i].gene
+         self:PrintOne(gene)
+         local conf = self:LoadGene(gene) 
+
+         local fitness = 999
+
+         if     self.nnType == "U" then fitness = trainU(self.train, self.test, conf)
+         elseif self.nnType == "V" then fitness = trainV(self.train, self.test, conf)           
+         else   
+            error("Invalid network type")
+         end
+
+         collectgarbage()
+
+         genes[i].score = fitness
+      end
+
+   else
+
    local njob = table.Count(genes)
    local pool = threads.Threads(
       nthread,
@@ -210,6 +279,7 @@ function NNGen:EvaluateAll(genes)
 
    pool:synchronize()
    pool:terminate() 
+   end
 
    return genes
 
@@ -240,8 +310,8 @@ function NNGen:Preconfigure(genConf)
       v[{{}, 2}]:add(-self.train.V.info[k].mean) --center input
    end
 
-
-   SHOW_PROGRESS = false
+   
+   SHOW_PROGRESS = true 
    USE_GPU       = true 
 
 end
